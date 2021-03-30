738d64409fbf4633bd7ac1549a30b20f8e69f7d1
Alexander Sakharov <sakharov487@mail.ru>
2021-03-30 21:20:18 +0300

    Written javadoc for Cashbox class

--- src/main/java/Cashbox.java
+++ src/main/java/Cashbox.java
@@ -1,21 +1,48 @@
+
 public class Cashbox {
     /**
      * Cash held at the cashbox
      */
     private volatile double cash;
 
+    /**
+     * Default constructor
+     */
     public Cashbox() {
     }
 
+    /**
+     * Constructor with parameters
+     *
+     * @param cash the amount of money kept in the cashbox
+     */
     public Cashbox(double cash) {
         this.cash = cash;
     }
 
-    public double getCash() {
-        return cash;
+    /**
+     * Аdding money to the cashbox
+     *
+     * @param sum the amount of money you need to put in the cashbox
+     */
+    public synchronized void put(double sum) {
+        this.cash += sum;
     }
 
-    public void setCash(double cash) {
-        this.cash = cash;
+    /**
+     * Taking money from the cashbox
+     *
+     * @param sum the amount of money to be withdrawn
+     * @return true if you managed to withdraw the required amount of money
+     */
+    public synchronized boolean withdraw(double sum) {
+        boolean operationResult;
+        if (cash - sum < 0) {
+            operationResult = false;
+        } else {
+            cash -= sum;
+            operationResult = true;
+        }
+        return operationResult;
     }
 }
